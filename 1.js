a32dc1315178cc108039e66b9dfd02eacf346da7
shuliqi <1169046371@qq.com>
2021-06-03 00:38:44 +0800

    算法

--- 1.js
+++ 1.js
@@ -840,3 +840,36 @@ function handle() {
   console.log("111");
 }
 window.addEventListener('resize', throttle(handle, 1000))
+
+
+
+var spiralOrder = function(matrix) {
+  if (!matrix.length || !matrix[0].length) {
+      return [];
+  }
+
+  const rows = matrix.length, columns = matrix[0].length;
+  const order = [];
+  let left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+  while (left <= right && top <= bottom) {
+      for (let column = left; column <= right; column++) {
+          order.push(matrix[top][column]);
+      }
+      for (let row = top + 1; row <= bottom; row++) {
+          order.push(matrix[row][right]);
+      }
+      console.log(left, right, top, bottom)
+      if (left < right && top < bottom) {
+          for (let column = right - 1; column > left; column--) {
+              order.push(matrix[bottom][column]);
+          }
+          for (let row = bottom; row > top; row--) {
+              order.push(matrix[row][left]);
+          }
+      }
+      [left, right, top, bottom] = [left + 1, right - 1, top + 1, bottom - 1];
+  }
+  return order;
+};
+
+console.log(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
